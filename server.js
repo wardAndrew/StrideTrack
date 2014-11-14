8b5df1ec6b1352cc30e96806d8ebc1461e32f34c
Andrew Ward <ward0196@Andrew-Wards-iMac-18.local>
2014-11-14 09:37:55 -0800

    Updated packages and server

--- server.js
+++ server.js
@@ -4,6 +4,11 @@
  */
 
 var express = require('express'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  errorHandler = require('errorHandler'),
+  static = require('serve-static'),
   http = require('http'),
   path = require('path'),
   applications = require('./routes/applications');
@@ -18,11 +23,11 @@ app.get('/applications/:id', applications.findApplicantById);
 app.get('/applications/:id/status', applications.findApplicantStatusById);
 app.post('/applications/:id/status', applications.updateApplicant);
 
-//app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
-//app.use(express.bodyParser());
-//app.use(express.methodOverride());
-//app.use(express.errorHandler());
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
+app.use(bodyParser.json()); /* parse application/json */
+app.use(methodOverride());
+app.use(errorHandler());
+app.use(static(path.join(__dirname, 'client')));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("StrideTrack server listening on port " + app.get('port'));
